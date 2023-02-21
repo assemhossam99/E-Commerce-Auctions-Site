e6ac95c5431c4c38b5eba96d82d8c7cf2aa33428
Assem <assem99.hossam@gmail.com>
2023-02-21 13:56:23 +0200

    added models

--- auctions/models.py
+++ auctions/models.py
@@ -17,7 +17,21 @@ class Listing(models.Model):
     startingBid = models.IntegerField()
     currentBid = models.IntegerField(null = True, blank = True)
     imageURL = models.URLField(max_length = 500, null = True, blank = True)
-    CategoryID = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = "listings", null = True, blank = True)
+    Category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = "listings", null = True, blank = True)
 
     def __str__(self):
         return f"{self.id}: {self.title} - {self.startingBid}$ - {self.currentBid}"
+
+class Comment(models.Model):
+    commentContent = models.CharField(max_length = 1024)
+    listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "comments")
+
+    def __str__(self):
+        return f"Comment {self.id} on Listing {self.Listing.id}: {self.commentContent}"
+
+class Bid(models.Model):
+    value = models.IntegerField()
+    listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "bids")
+
+    def __str__(self):
+        return f"Bid {self.id} on Listing {self.Listing.id}: {self.value}"
