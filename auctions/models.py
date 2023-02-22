0ea078762b468ed30ccb3d62401081a8071b7cbd
Assem <assem99.hossam@gmail.com>
2023-02-22 17:44:11 +0200

    added user to comments and bids in models.py

--- auctions/models.py
+++ auctions/models.py
@@ -26,13 +26,15 @@ class Listing(models.Model):
 class Comment(models.Model):
     commentContent = models.CharField(max_length = 1024)
     listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self):
-        return f"Comment {self.id} on Listing {self.Listing.id}: {self.commentContent}"
+        return f"Comment {self.id} on Listing {self.listing.id}: {self.commentContent}"
 
 class Bid(models.Model):
     value = models.IntegerField()
     listing = models.ForeignKey(Listing, on_delete = models.CASCADE, related_name = "bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
 
     def __str__(self):
-        return f"Bid {self.id} on Listing {self.Listing.id}: {self.value}"
+        return f"Bid {self.id} on Listing {self.listing.id}: {self.value}"
