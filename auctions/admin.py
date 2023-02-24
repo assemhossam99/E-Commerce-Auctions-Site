4e45669822f3a657930077678a5aa6e54b44657d
Assem <assem99.hossam@gmail.com>
2023-02-24 14:54:00 +0200

    added style to categories page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Listing, Category, Comment, Bid, WatchList
+from .models import Listing, Category, Comment, Bid, WatchList, User
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Bid)
 admin.site.register(WatchList)
+admin.site.register(User)
